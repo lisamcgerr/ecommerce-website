86cbf88bf3f1e37dfd267c23c4e6d7839b0f1403
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-07-28 10:55:44 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC68JZJS9KhZ4Ro_L22JjD09iMuPNiFsnA",
+    authDomain: "ecommerce-website-347a6.firebaseapp.com",
+    projectId: "ecommerce-website-347a6",
+    storageBucket: "ecommerce-website-347a6.appspot.com",
+    messagingSenderId: "177321378494",
+    appId: "1:177321378494:web:9e62fdafe871f5b70214ef",
+    measurementId: "G-T4ZPSE0ZRZ"
+  };
