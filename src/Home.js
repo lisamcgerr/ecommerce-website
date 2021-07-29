75da08447e3aaf2b06943df3fce5e9baa6062a06
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-07-28 19:11:51 -0500

    filler products added, fixed margin-bottom of .home__image

--- src/Home.js
+++ src/Home.js
@@ -9,18 +9,18 @@ function Home() {
                 <img 
                 className='home__image' 
                 src='https://user-images.githubusercontent.com/73184313/127373370-a925e758-eba5-461f-834e-2fe14498a6e1.jpg ' 
-                alt = 'grey paint background' />
+                alt='grey paint background' />
                 <div className='home__row'>
                     <Product />
-                    {/* Product */}
+                    <Product />
                 </div>
                 <div className='home__row'>
-                    {/* Product */}
-                    {/* Product */}
-                    {/* Product */}
+                    <Product />
+                    <Product />
+                    <Product />
                 </div>
                 <div className='home__row'>
-                    {/* Product */}
+                    <Product />
                 </div>
             </div>
         </div>
