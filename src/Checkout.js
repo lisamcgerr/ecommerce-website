ae4106b75f3ac838eec6d4c3d33bd95a41ac328b
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-08-26 15:10:04 -0500

    added banner image to checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,8 @@ function Checkout(){
     return (
         <div className='checkout'>
             <div className='checkout__left'>
-                {/* <img className='checkout__ad' src='' alt='' /> */}
+                <img className='checkout__ad' src='https://user-images.githubusercontent.com/73184313/131029015-fa2d4214-8a04-4480-b9c7-67a3ceed3384.png' alt='grey textured background that reads created by Lisa McGerr Full Stack Web Developer' />
+                    {/* add click listener to hyperlink to portfolio */}
 
                 <div>
                     <h2 className='checkout__title'>
