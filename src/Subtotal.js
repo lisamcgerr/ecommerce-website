61f0bedd8a85e5cf59139c4296d23626f6559bf8
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-08-26 16:07:21 -0500

    pushing minor changes from subtotal.js and .css

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './Subtotal.css'
+import CurrencyFormat from 'react-currency-format'
 
 function Subtotal() {
     return (
         <div className='subtotal'>
-            
+            <CurrencyFormat 
+                renderText={(value) => (
+                    <>
+                        <p>
+                            {/* value needs to be updated */}
+                            Subtotal (0 items): <strong>0</strong>
+                        </p>
+                        <small className='subtotal__gift'>
+                            <input type='checkbox' /> 
+                            This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={0} //input value
+                displayType={'text'}
+                thousandSeparator={true}
+                prefix={'$'}
+            /> 
+           <button>Proceed to Checkout</button> 
         </div>
     )
 }
