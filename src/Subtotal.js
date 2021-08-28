f6bb7ad27e5e72e090263fbd45c1bcada23344e6
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-08-28 15:06:09 -0500

    subtotal and item total in basket working

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format'
 import { useStateValue } from './StateProvider'
+import { getBasketTotal } from './reducer'
 
 function Subtotal() {
     //get the basket and
@@ -14,7 +15,7 @@ function Subtotal() {
                     <>
                         <p>
                             {/* value needs to be updated */}
-                            Subtotal {basket.length} items: <strong>{basket.length}</strong>
+                            Subtotal {basket.length} items: <strong>{value}</strong>
                         </p>
                         <small className='subtotal__gift'>
                             <input type='checkbox' /> 
@@ -23,7 +24,7 @@ function Subtotal() {
                     </>
                 )}
                 decimalScale={2}
-                value={0} //input value
+                value={getBasketTotal(basket)} 
                 displayType={'text'}
                 thousandSeparator={true}
                 prefix={'$'}
--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,8 @@ export const initialState = {
     basket: [],
 }
 
-//selector
+//selector - add the item price to the total amount and the starting value is 0
+//reduce() iterates through the basket and gets the total amount 
 export const getBasketTotal = ( basket ) => 
     basket?.reduce((amount, item) => item.price + amount, 0)
 
