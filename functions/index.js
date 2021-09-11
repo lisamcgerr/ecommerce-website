3dbc9a98f3ae8cb0a93dfe24dd5454ee7483f49b
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-09-11 13:10:32 -0500

    deployed app on firebase https://ecommerce-website-347a6.web.app

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,20 @@
-const functions = require("firebase-functions");
+// const functions = require('firebase-functions')
+// const express = require('express')
+// const cors = require('cors')
+// const stripe = require('stripe')
+// ('pk_test_51JXqXUH6myHdM06N0pPqE5GGUb2SqA
+// HboNdolxPEJw7OPHtWPeFKBQwv8HRPqqvqfCNqeN3Ioa1HAlolKbGGQdAU00DcvKz1o1')
 
-const express = require('express')
+// //api
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// //app config
+// const app =express()
+
+// //middleware
+// app.use(cors({ origin: true }))
+// app.use(express.json())
+
+// //api routes
+// app.get('/', (request, response) => res.status(200).send('hello world'))
+
+// //listen command
